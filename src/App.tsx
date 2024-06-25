26a1c0196deab8c92289ac74b65c37bf93e0d0e6
Ancel Guarayo <aguarayo@gmail.com>
2024-06-25 17:40:21 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom"
+import ContactForm from "./_auth/forms/ContactForm"
+import Home from './_root/pages/Home'
+import AuthLayout from "./_auth/AuthLayout"
+import RootLayout from "./_root/RootLayout"
+
+const App = () => {
+  return (
+    <main>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          { /** Public routes */}
+          <Route path="/contact" element={<ContactForm />} />
+          {/** Private routes */}
+        </Route>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  )
+}
+
+export default App
