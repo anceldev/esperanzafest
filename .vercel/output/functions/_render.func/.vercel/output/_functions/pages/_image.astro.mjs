cf25f5bb6741a5873cb958bdc94216b0c4b922d5
Ancel Guarayo <aguarayo@gmail.com>
2024-07-03 23:49:58 +0200

    fix: Form created

--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('../chunks/generic_BSyT4MNL.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/pages/contacto.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('../chunks/contacto_CLa9Ye0M.mjs');
-
-export { page };
