391fc643bfc7418c50a75e6f7c383a9cb93ea13f
Ancel Guarayo <aguarayo@gmail.com>
2025-06-22 19:32:34 +0200

    Turso db implemented

--- db/config.ts
+++ db/config.ts
@@ -1,15 +1,6 @@
-import { column, defineDb, defineTable } from 'astro:db';
+import { column, defineDb, defineTable } from "astro:db";
 
 // https://astro.build/db/config
-
-const Contacto = defineTable({
-  columns: {
-    id: column.text(),
-    name: column.text(),
-    email: column.text(),
-    phone: column.text()
-  },
-});
 const Contact = defineTable({
   columns: {
     name: column.text(),
@@ -19,8 +10,4 @@ const Contact = defineTable({
 });
 
 export default defineDb({
-  tables: { 
-    Contact,
-    Contacto,
-  }
-});
+  tables: { Contact }});
